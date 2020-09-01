01e2c63f44e35cddd97a542858e6163d1997688e
jerry <309032663@qq.com>
2020-09-01 10:57:27 +0800

    update

---
+++ app/src/main/java/cn/cxy/customviews/fall/FallingView2.kt
@@ -0,0 +1,66 @@
+package cn.cxy.customviews.fall
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import kotlin.math.min
+
+/**
+ * 圆点飘落，每次降落前x坐标会改变
+ */
+class FallingView2(context: Context, attrs: AttributeSet?) : View(context, attrs) {
+    private var mViewWidth = 0
+    private var mViewHeight = 0
+    private var mPaint = Paint()
+    private var snowX = 100f
+    private var snowY = 0
+    private var radius = 25f
+    private val defaultWidth = 600 //默认宽度
+    private val defaultHeight = 1000 //默认高度
+    private val intervalTime = 5 //重绘间隔时间
+
+    init {
+        mPaint.color = Color.WHITE
+        mPaint.style = Paint.Style.FILL
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val height = measureSize(defaultHeight, heightMeasureSpec)
+        val width = measureSize(defaultWidth, widthMeasureSpec)
+        setMeasuredDimension(width, height)
+        mViewWidth = width
+        mViewHeight = height
+    }
+
+    private fun measureSize(defaultSize: Int, measureSpec: Int): Int {
+        var result = defaultSize
+        val specMode = MeasureSpec.getMode(measureSpec)
+        val specSize = MeasureSpec.getSize(measureSpec)
+        if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize
+        } else if (specMode == MeasureSpec.AT_MOST) {
+            result = min(result, specSize)
+        }
+        return result
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawCircle(snowX, snowY.toFloat(), radius, mPaint)
+        handler.postDelayed(runnable, intervalTime.toLong()) //间隔一段时间再进行重绘
+    }
+
+    // 重绘线程
+    private val runnable = Runnable {
+        snowY += 15
+        if (snowY > mViewHeight) { //超出屏幕则重置雪球位置
+            snowY = 0
+            snowX = (50..mViewWidth - 50).random().toFloat()
+        }
+        invalidate()
+    }
+}
---
+++ app/src/main/java/cn/cxy/customviews/fall/type3/FallObject.kt
@@ -0,0 +1,104 @@
+package cn.cxy.customviews.fall.type3
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import java.util.*
+
+class FallObject {
+    private var initX = 0
+    private var initY = 0
+    private var random: Random? = null
+    private var parentWidth = 0 //父容器宽度
+    private var parentHeight = 0 //父容器高度
+    private var objectWidth = 0f //下落物体宽度
+    private var objectHeight = 0f //下落物体高度
+    private var initSpeed = 0 //初始下降速度
+    private var presentX = 0f //当前位置X坐标
+    private var presentY = 0f //当前位置Y坐标
+    private var presentSpeed = 0f //当前下降速度
+    private var bitmap: Bitmap
+    lateinit var builder: Builder
+
+    constructor(builder: Builder, parentWidth: Int, parentHeight: Int) {
+        random = Random()
+        this.parentWidth = parentWidth
+        this.parentHeight = parentHeight
+        initX = random!!.nextInt(parentWidth) //随机物体的X坐标
+        initY = random!!.nextInt(parentHeight) - parentHeight //随机物体的Y坐标，并让物体一开始从屏幕顶部下落
+        presentX = initX.toFloat()
+        presentY = initY.toFloat()
+        initSpeed = builder.initSpeed
+        presentSpeed = initSpeed.toFloat()
+        bitmap = builder.bitmap
+        objectWidth = bitmap.width.toFloat()
+        objectHeight = bitmap.height.toFloat()
+    }
+
+    private constructor(builder: Builder) {
+        this.builder = builder
+        initSpeed = builder.initSpeed
+        bitmap = builder.bitmap
+    }
+
+    class Builder(bitmap: Bitmap) {
+        var initSpeed: Int
+        val bitmap: Bitmap
+
+        /**
+         * 设置物体的初始下落速度
+         * @param speed
+         * @return
+         */
+        fun setSpeed(speed: Int): Builder {
+            initSpeed = speed
+            return this
+        }
+
+        fun build(): FallObject {
+            return FallObject(this)
+        }
+
+        init {
+            initSpeed = defaultSpeed
+            this.bitmap = bitmap
+        }
+    }
+
+    /**
+     * 绘制物体对象
+     * @param canvas
+     */
+    fun drawObject(canvas: Canvas) {
+        moveObject()
+        canvas.drawBitmap(bitmap, presentX, presentY, null)
+    }
+
+    /**
+     * 移动物体对象
+     */
+    private fun moveObject() {
+        moveY()
+        if (presentY > parentHeight) {
+            reset()
+        }
+    }
+
+    /**
+     * Y轴上的移动逻辑
+     */
+    private fun moveY() {
+        presentY += presentSpeed
+    }
+
+    /**
+     * 重置object位置
+     */
+    private fun reset() {
+        presentY = -objectHeight
+        presentSpeed = initSpeed.toFloat()
+    }
+
+    companion object {
+        private const val defaultSpeed = 10 //默认下降速度
+    }
+}
